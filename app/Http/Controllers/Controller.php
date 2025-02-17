eac39dfd169275820232c4e074b535a3304a62d5
Strory16 <auliyaazzahraazzahra@gmail.com>
2025-02-17 20:33:27 +0700

    controller.k

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,9 @@
 <?php
 
+// Menentukan namespace untuk mengorganisir kode
 namespace App\Http\Controllers;
 
+// Mendefinisikan kelas Controller sebagai kelas abstrak
 abstract class Controller
 {
     //
