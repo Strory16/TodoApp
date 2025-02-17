f80cab81eacb4a37e36d01598293b4b46d75df16
Strory16 <auliyaazzahraazzahra@gmail.com>
2025-02-17 20:44:57 +0700

    taskc

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,6 +1,7 @@
 <?php // Tag pembuka untuk kode PHP
 
 // Menentukan namespace untuk mengorganisir kode
+// Dan untuk menghindari konflik nama antara kelas, fungsi, atau konstanta yang mungkin memiliki nama yang sama di bagian lain dari aplikasi.
 namespace App\Http\Controllers;
 
 // Mendefinisikan kelas Controller sebagai kelas abstrak
--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,6 +1,9 @@
-<?php
+<?php // Tag pembuka untuk kode PHP
 
+// Menentukan namespace untuk mengorganisir kode
+// Dan untuk menghindari konflik nama antara kelas, fungsi, atau konstanta yang mungkin memiliki nama yang sama di bagian lain dari aplikasi.
 namespace App\Http\Controllers;
+
 
 use App\Models\Task;
 use App\Models\TaskList;
