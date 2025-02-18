9985a97b34352a1f8b5db0d9265d0bbe63d7091b
Strory16 <auliyaazzahraazzahra@gmail.com>
2025-02-18 19:31:03 +0700

    tasklist

--- app/Models/TaskList.php
+++ app/Models/TaskList.php
@@ -9,13 +9,15 @@ use Illuminate\Database\Eloquent\Model;
 // Mendefinisikan kelas TaskList yang merupakan turunan dari Model Eloquent
 class TaskList extends Model
 {
+    // Menentukan atribut yang dapat diisi secara massal
     protected $fillable = ['name'];
-    protected $guarded = [
-        'id',
-        'created_at',
-        'updated_at'
-    ];
 
+    // Menentukan atribut yang tidak dapat diisi secara massal
+    protected $guarded = [
+        'id',          // ID dari TaskList, tidak dapat diisi secara massal
+        'created_at',  // Tanggal dan waktu saat TaskList dibuat, tidak dapat diisi secara massal
+        'updated_at'   // Tanggal dan waktu saat TaskList terakhir diperbarui, tidak dapat diisi secara massal
+    ];
     public function tasks() {
         return $this->hasMany(Task::class, 'list_id');
     }
